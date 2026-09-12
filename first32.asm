56ee1f9b3fea466e219630ab8ea054e7c994e55e
Fitoliaka <119758687+Fitoliaka@users.noreply.github.com>
2026-09-12 17:02:50 +0000

    hello, world asm, 64, 32

--- first32.asm
+++ first32.asm
@@ -1,20 +1,16 @@
-format ELF executable
-entry _start
-
-segment readable executable
+format ELF
+public _start
+msg db "Hello, world", 0xA, 0
 
 _start:
     ; write(1, msg, msg_len)
     mov eax, 4
     mov ebx, 1
     mov ecx, msg
-    mov edx, msg_len
+    mov edx, 14
     int 0x80
 
     ; exit(0)
     mov eax, 1
     xor ebx, ebx
     int 0x80
-
-msg db "Hello, world", 10
-msg_len = $ - msg
